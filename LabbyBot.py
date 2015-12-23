746e896f551cbe2376f05612aff450b51f4a5942
Niall Robinson <niall.robinson@informaticslab.co.uk>
2015-12-23 11:02:32 +0000

    interctive

--- LabbyBot.py
+++ LabbyBot.py
@@ -18,14 +18,14 @@ class LabbyBot:
     def bringToLife(self):
         while True:
             result = BrickPiUpdateValues()  # Ask BrickPi to update values for sensors/motors
-            if not result:
-                '''
-                maybe move the logic later on
-                '''
-                if not self.edgeDetected():
-                    self.biTrack.setSpeed(200)
-                else:
-                    self.biTrack.setSpeed(0)
+#            if not result:
+#                '''
+#                maybe move the logic later on
+#                '''
+#                if not self.edgeDetected():
+#                   self.biTrack.setSpeed(200)
+#                else:
+#                    self.biTrack.setSpeed(0)
             time.sleep(.1)
 
 
@@ -33,6 +33,6 @@ class LabbyBot:
         return self.irSensor.getValue() >= limit
 
 
-if __name__=='__main__':
-    myRobot = LabbyBot(leftTrackPort=PORT_D, rightTrackPort=PORT_A, irSensorPort=PORT_1)
-    myRobot.bringToLife()
+#if __name__=='__main__':
+myRobot = LabbyBot(leftTrackPort=PORT_D, rightTrackPort=PORT_A, irSensorPort=PORT_1)
+#    myRobot.bringToLife()
